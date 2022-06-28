053f8a909fe8a8bb7223af51573c687cef321017
Jkirbs86 <51921860+Jkirbs86@users.noreply.github.com>
2022-06-27 21:18:34 -0600

    Add files via upload

---
+++ Challenge2/js-birthday/js/script.js
@@ -0,0 +1,3 @@
+var username = prompt("Enter your name:");
+var age = prompt("Enter your age?:");
+alert("Happy Birthday  " + username + "! you are " + age + "  Years Old." +   "Have Some Cake!");
---
+++ Challenge2/mad-lib/js/script.js
@@ -0,0 +1,14 @@
+var noun = prompt("enter noun,person:");
+var noun2 = prompt ("enter noun,person:");
+var adjective = prompt ("enter adjective");
+var adjective2 = prompt ("enter adjective");
+var  verb = prompt("enter verb");
+var verb2 = prompt("enter verb");
+
+
+
+
+alert("Albert Einstein, the son of "  + noun  +   " and  "   + noun2 +   " was born in Ulm Germany in 1879 In 1902 he had a job as assistant House in the Swiss patent office and attended the University of Zurich There he began studying atoms and molecules. He developed the theory of  "  + adjective  + "relativity, which expanded the phenomena of sub-atomic and"  + adjective2 + "magnetism. In 1921, he won the Nobel prize for s.and was director oftheoretical physics at the Kaiser Wilhelm s.in Berlin.In 1933, when Hitler became Chancellor of Germany, Einstein came to America to  "  + verb + "  a post at Princeton Institute. Where his theories helped America" + verb2 +  " the first atomic s. There is no question about it Einstein was one of the most brilliant influences of our time.");
+
+
+
